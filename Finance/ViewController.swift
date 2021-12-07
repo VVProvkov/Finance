a68ebce3dac849ebe95e4e3214f6c9e30274fd13
Vadim <newprovkov@yandex.ru>
2021-12-07 09:53:06 +0300

    Initial Commit

---
+++ Finance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance
+//
+//  Created by Vadim on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
